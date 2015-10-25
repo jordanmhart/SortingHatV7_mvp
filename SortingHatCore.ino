92b32d99c3030e7ad65943ca3b6ea49d9343c8c7
Jordan Hart <jordan@jordanhart.com>
2015-10-25 12:31:12 -0700

    integrated servo refactor

--- SortingHatCore.ino
+++ SortingHatCore.ino
@@ -10,9 +10,18 @@ Servo middleEyebrow; // lower=?, middle=?, upper=?
 unsigned long leftEyebrowTimeout = 0;
 unsigned long rightEyebrowTimeout = 0;
 unsigned long middleEyebrowTimeout = 0;
+unsigned long leftEyebrowStartTimeout = 0;
+unsigned long rightEyebrowStartTimeout = 0;
+unsigned long middleEyebrowStartTimeout = 0;
+int leftEyebrowTarget = 90;
+int rightEyebrowTarget = 90;
+int middleEyebrowTarget = 90;
 int leftEyebrowDefault = 90;
 int rightEyebrowDefault = 90;
 int middleEyebrowDefault = 90;
+bool leftEyebrowDone = true;
+bool rightEyebrowDone = true;
+bool middleEyebrowDone = true;
 
 //Eyes Variables
 int redPin;
@@ -50,10 +59,12 @@ void question(){
 }
 
 void thinking(){
-  eyebrows_setLeftFor(140, 3000);
-  eyebrows_setRightFor(100, 3000);
-  eyeBlink_greenFor(3000);
-  eye_whiteFor(3000);
+  eyebrows_setLeftFor(140, 1000);
+  eyebrows_setRightFor(100, 1000);
+  eyebrows_setLeftForAfter(80, 1000, 3000);
+  eyebrows_setRightForAfter(150, 1500, 4500);
+  eyeBlink_greenFor(6000);
+  eye_whiteFor(6000);
 }
 
 void gryffindor(){
@@ -99,19 +110,34 @@ void eyebrows_setup(uint8_t leftBrowPin, uint8_t rightBrowPin, uint8_t middleBro
 void eyebrows_update() {
   unsigned long now = millis();
 
+  if (leftEyebrowStartTimeout != 0 && now > leftEyebrowStartTimeout) {
+    leftEyebrow.write(leftEyebrowTarget);
+    leftEyebrowStartTimeout = 0;
+  }
   if (leftEyebrowTimeout != 0 && now > leftEyebrowTimeout) {
     eyebrows_setLeft(leftEyebrowDefault);
     leftEyebrowTimeout = 0;
+    leftEyebrowDone = true;
   }
 
+  if (rightEyebrowStartTimeout != 0 && now > rightEyebrowStartTimeout) {
+    rightEyebrow.write(rightEyebrowTarget);
+    rightEyebrowStartTimeout = 0;
+  }
   if (rightEyebrowTimeout != 0 && now > rightEyebrowTimeout) {
     eyebrows_setRight(rightEyebrowDefault);
     rightEyebrowTimeout = 0;
+    rightEyebrowDone = true;
   }
 
+  if (middleEyebrowStartTimeout != 0 && now > middleEyebrowStartTimeout) {
+    middleEyebrow.write(middleEyebrowTarget);
+    middleEyebrowStartTimeout = 0;
+  }
   if (middleEyebrowTimeout != 0 && now > middleEyebrowTimeout) {
     eyebrows_setMiddle(middleEyebrowDefault);
     middleEyebrowTimeout = 0;
+    middleEyebrowDone = true;
   }
 }
 
@@ -120,6 +146,13 @@ void eyebrows_setLeftDefault(int pos) { leftEyebrowDefault = pos; }
 void eyebrows_setLeftFor(int pos, unsigned long duration) {
   leftEyebrow.write(pos);
   leftEyebrowTimeout = millis() + duration;
+  leftEyebrowDone = false;
+}
+void eyebrows_setLeftForAfter(int pos, unsigned long duration, unsigned long waitTime) {
+  leftEyebrowStartTimeout = millis() + waitTime;
+  leftEyebrowTimeout = leftEyebrowStartTimeout + duration;
+  leftEyebrowTarget = pos;
+  leftEyebrowDone = false;
 }
 
 void eyebrows_setRight(int pos) { rightEyebrow.write(pos); }
@@ -127,6 +160,13 @@ void eyebrows_setRightDefault(int pos) { rightEyebrowDefault = pos; }
 void eyebrows_setRightFor(int pos, unsigned long duration) {
   rightEyebrow.write(pos);
   rightEyebrowTimeout = millis() + duration;
+  rightEyebrowDone = false;
+}
+void eyebrows_setRightForAfter(int pos, unsigned long duration, unsigned long waitTime) {
+  rightEyebrowStartTimeout = millis() + waitTime;
+  rightEyebrowTimeout = rightEyebrowStartTimeout + duration;
+  rightEyebrowTarget = pos;
+  rightEyebrowDone = false;
 }
 
 void eyebrows_setMiddle(int pos) { middleEyebrow.write(pos); }
@@ -134,6 +174,13 @@ void eyebrows_setMiddleDefault(int pos) { middleEyebrowDefault = pos; }
 void eyebrows_setMiddleFor(int pos, unsigned long duration) {
   middleEyebrow.write(pos);
   middleEyebrowTimeout = millis() + duration;
+  middleEyebrowDone = false;
+}
+void eyebrows_setMiddleForAfter(int pos, unsigned long duration, unsigned long waitTime) {
+  middleEyebrowStartTimeout = millis() + waitTime;
+  middleEyebrowTimeout = middleEyebrowStartTimeout + duration;
+  middleEyebrowTarget = pos;
+  middleEyebrowDone = false;
 }
 
 //Eyes Functions
